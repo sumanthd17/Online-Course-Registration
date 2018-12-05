aad0dd1cbc10689ab62d878ee81969c84fecd3b0
Rusheel Koushik Gollakota <koushikgns@gmail.com>
2018-12-05 17:18:28 +0530

    Add files via upload

--- OnlineCourseRegistration/OnlineCourseRegistration/wsgi.py
+++ OnlineCourseRegistration/OnlineCourseRegistration/wsgi.py
@@ -11,7 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
+ 
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineCourseRegistration.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
